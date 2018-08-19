5d8e94dbfa816c8c7c35605cd514a0ec2b89a99d
Obafemi Fadairo <ofpupp@princeton.edu>
2018-08-19 00:07:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ class BoardMember {
     return "You will see great things from Scuber."
   }
 
-  () {
+  f() {
     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
   }
 }
